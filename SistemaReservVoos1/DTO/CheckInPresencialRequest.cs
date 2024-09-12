f3a60918da2d7ad577586f1d392f6cc55cadcdb6
Pedro Henrique Alexandre <121180098+phalexandre10@users.noreply.github.com>
2024-09-12 20:25:19 -0300

    Adicionando projeto

---
+++ SistemaReservVoos1/DTO/CheckInPresencialRequest.cs
@@ -0,0 +1,11 @@
+﻿using SistemaReservasVoos.Modelos;
+
+namespace SistemaReservasVoos.DTO
+{
+    public class CheckInPresencialRequest
+    {
+        public int ReservaId { get; set; }
+        public string NumeroAssento { get; set; }
+        public Passageiro DadosAtualizados { get; set; }
+    }
+}
---
+++ SistemaReservVoos1/Migrations/20240912005516_Projeto.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SistemaReservasVoos.Migrations
+{
+    /// <inheritdoc />
+    public partial class Projeto : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Passageiros",
+                columns: table => new
+                {
+                    CPF = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Endereco = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Celular = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    TelefoneFixo = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Passageiros", x => x.CPF);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Voos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Origem = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Destino = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DataPartida = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Horario = table.Column<TimeSpan>(type: "time", nullable: false),
+                    Companhia = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Preco = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    AssentosDisponiveis = table.Column<int>(type: "int", nullable: false),
+                    CapacidadeTotal = table.Column<int>(type: "int", nullable: false, defaultValue: 100)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Voos", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Reservas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CPFPassageiro = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    VooId = table.Column<int>(type: "int", nullable: false),
+                    DataReserva = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    QuantidadePassageiros = table.Column<int>(type: "int", nullable: false),
+                    NumeroAssento = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CheckInRealizado = table.Column<bool>(type: "bit", nullable: false),
+                    FormaPagamento = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Reservas", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Reservas_Passageiros_CPFPassageiro",
+                        column: x => x.CPFPassageiro,
+                        principalTable: "Passageiros",
+                        principalColumn: "CPF",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Reservas_Voos_VooId",
+                        column: x => x.VooId,
+                        principalTable: "Voos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reservas_CPFPassageiro",
+                table: "Reservas",
+                column: "CPFPassageiro");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reservas_VooId",
+                table: "Reservas",
+                column: "VooId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Reservas");
+
+            migrationBuilder.DropTable(
+                name: "Passageiros");
+
+            migrationBuilder.DropTable(
+                name: "Voos");
+        }
+    }
+}
